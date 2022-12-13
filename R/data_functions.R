705ef5261ad115463edf21d111ef1bf4d6a3217c
GrahamBuhrman <57652495+GrahamBuhrman@users.noreply.github.com>
2022-12-13 12:32:08 -0600

    update package

--- R/data_functions.R
+++ R/data_functions.R
@@ -162,16 +162,19 @@ diagnostic_plots <- function(model,
 
 # MLR Results Table Function ----
 
-mlr_table <- function(model, var_names) {
+reg_table <- function(model, hide = NULL, adjust_method = "none", var_cov = NULL, exp_est = FALSE) {
 
   #' Generate MLR results table
   #'
   #' Given a multivariate linear regression model and the labels for parameters, create a results table
   #'
-  #' @param model The MLR model whose results should be used
-  #' @param var_names A named list with entries of the format parameter = "Parameter Label"
+  #' @param model Model object, of class "lm" or "glm".
+  #' @param hide Character vector, if not NULL, indicates which model statistics to be hidden in the table. Possible statistics include: "term", "estimate", "std.error", conf.level", "conf.low", "conf.high", "statistic", "df.error", "p.value".
+  #' @param adjust_method = Character vector, if not NULL, indicates the method to adjust p-values. See stats::p.adjust() for details. Further possible adjustment methods are "tukey", "scheffe", "sidak" and "none" to explicitly disable adjustment for emmGrid objects (from emmeans).
+  #' @param var_cov = variance-covariance matrix used to compute uncertainty estimates (e.g., for robust standard errors). This argument accepts a covariance matrix, a function which returns a covariance matrix, or a string which identifies the function to be used to compute the covariance matrix.
+  #' @exp_est = Logical, indicating whether or not to exponentiate the coefficients (and related confidence intervals). This is typical for logistic regression, or more generally speaking, for models with log or logit links. It is also recommended to use exponentiate = TRUE for models with log-transformed response values. Note: Delta-method standard errors are also computed (by multiplying the standard errors by the transformed coefficients). This is to mimic behaviour of other software packages, such as Stata, but these standard errors poorly estimate uncertainty for the transformed coefficient. The transformed confidence interval more clearly captures this uncertainty. For compare_parameters(), exponentiate = "nongaussian" will only exponentiate coefficients from non-Gaussian families.
   #'
-  #' @return A gt table object
+  #' @return A gt table object of the model summary.
   #' @importFrom rlang .data
   #' @export
   #'
@@ -181,53 +184,129 @@ mlr_table <- function(model, var_names) {
   #'
   #' data(mtcars)
   #'
+  #' # for a model of class "lm"
+  #'
   #' m1 <- stats::lm(mpg ~ wt + am, data = mtcars)
   #'
-  #' # make named list of parameter labels
+  #' reg_table(m1)
   #'
-  #' parameter_names <- list(wt = "Weight", am = "Transmission")
+  #' # for a model of class "glm"
   #'
-  #' # generate results table
+  #' m2 <- stats::glm(vs ~ wt + am, data = mtcars, family = "binomial")
   #'
-  #' mlr_table(model = m1, var_names = parameter_names)
+  #' reg_table(m2)
   #'
 
   # generate results table for multivariate linear regression model
 
-  # get model fit statistics
+    if (class(model)[1] == "lm"){
 
-  fit_stats <-
-    broom::glance(model) %>%
-    dplyr::select(`R<sup>2</sup>` = .data$r.squared,
-                  `Adj R<sup>2</sup>` = .data$adj.r.squared,
-                  .data$AIC,
-                  .data$BIC) %>%
-    dplyr::mutate(`f<sup>2</sup>` = .data$`R<sup>2</sup>` / (1 - .data$`R<sup>2</sup>`)) %>%
-    dplyr::mutate_all(function(x) gtsummary::style_sigfig(x, digits = 3)) %>%
-    {paste(names(.data), .data, sep = " = ", collapse = "; ")}
+      fit_stats <-
+        broom::glance(model) %>%
+        dplyr::select(`R<sup>2</sup>` = .data$r.squared,
+                      `Adj R<sup>2</sup>` = .data$adj.r.squared,
+                      .data$AIC,
+                      .data$BIC) %>%
+        dplyr::mutate(`f<sup>2</sup>` = .data$`R<sup>2</sup>` / (1 - .data$`R<sup>2</sup>`)) %>%
+        dplyr::mutate_all(function(x) gtsummary::style_sigfig(x, digits = 3)) %>%
+        {paste(names(.data), .data, sep = " = ", collapse = "; ")}
 
-  # generate results table
+      table <-
+        parameters::model_parameters(model = model,
+                                     vcov = var_cov,
+                                     p_adjust = adjust_method) %>%
+        insight::standardize_names("broom") %>%
+        insight::standardize_column_order("easystats") %>%
+        tibble::tibble() %>%
+        gt::gt() %>%
+        gt::fmt_number(
+          columns = c(estimate,
+                      std.error,
+                      conf.level,
+                      conf.low,
+                      conf.high,
+                      statistic,
+                      df.error,
+                      p.value),
+          decimals = 3
+        ) %>%
+        gt::tab_style(
+          style = list(
+            gt::cell_text(weight = "bold")
+          ),
+          locations = list(
+            gt::cells_body(
+              columns = p.value,
+              rows = p.value < 0.05
+            ),
+            gt::cells_column_labels()
+          )
+        ) %>%
+        gt::tab_source_note(gt::html(fit_stats))
 
-  table <-
-    gtsummary::tbl_regression(
-      x = model,
-      tidy_fun = function(x, conf.int = TRUE, conf.level = 0.95) {
-        parameters::model_parameters(
-          model = x,
-          vcov = "vcovHC") %>%
-        insight::standardize_names(style = "broom") %>%
+      if (!is.null(hide)) {
+
+        table <-
+          table %>%
+          gt::cols_hide(columns = hide)
+
+      }
+
+    } else if (class(model)[1] == "glm") {
+
+      foo <-
+        data.frame(`Tjur's R<sup>2</sup>` = performance::r2_tjur(model)) %>%
         tibble::tibble()
-        },
-      label = var_names,
-      intercept = TRUE) %>%
-    gtsummary::bold_labels() %>%
-    gtsummary::add_vif() %>%
-    gtsummary::add_q(method = "bonferroni") %>%
-    gtsummary::bold_p(q = TRUE) %>%
-    gtsummary::modify_column_unhide(column = .data$std.error) %>%
-    gtsummary::as_gt() %>%
-    gt::tab_source_note(gt::html(fit_stats))
 
-  return(table)
+      fit_stats <-
+        foo %>%
+        dplyr::mutate_all(function(x) gtsummary::style_sigfig(x, digits = 3)) %>%
+        {paste(names(.data), .data, sep = " = ", collapse = "; ")}
 
-}
+      table <-
+        parameters::model_parameters(model = model,
+                                     exponentiate = exp_est,
+                                     vcov = var_cov,
+                                     p_adjust = adjust_method) %>%
+        insight::standardize_names("broom") %>%
+        insight::standardize_column_order("easystats") %>%
+        tibble::tibble() %>%
+        gt::gt() %>%
+        gt::fmt_number(
+          columns = c(estimate,
+                      std.error,
+                      conf.level,
+                      conf.low,
+                      conf.high,
+                      statistic,
+                      df.error,
+                      p.value),
+          decimals = 3
+        ) %>%
+        gt::tab_style(
+          style = list(
+            gt::cell_text(weight = "bold")
+          ),
+          locations = list(
+            gt::cells_body(
+              columns = p.value,
+              rows = p.value < 0.05
+            ),
+            gt::cells_column_labels()
+          )
+        ) %>%
+        gt::tab_source_note(gt::html(fit_stats))
+
+      if (!is.null(hide)) {
+
+        table <-
+          table %>%
+          gt::cols_hide(columns = hide)
+
+      }
+
+    }
+
+    return(table)
+
+  }
